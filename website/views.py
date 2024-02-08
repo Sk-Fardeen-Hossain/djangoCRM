36b25a34d8cc96962c0c7bcac2ec038038c189c5
Sk <evaD8886@gmail.com>
2024-02-08 13:39:40 +0530

    added individual records page

--- website/views.py
+++ website/views.py
@@ -22,12 +22,12 @@ def home(request):
             return redirect('home')
     else:
         return render(request, 'home.html',{'records':records})
-def login_user(request):
-    pass
+    
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
+
 def register_user(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
@@ -44,3 +44,11 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html',{'form':form})
     return render(request, 'register.html',{'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"You must be logged in to view records..")
+        return redirect('home')
